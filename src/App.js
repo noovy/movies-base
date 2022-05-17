d7de62840be52f701ede79fa2217b28e91e90338
Michał Nowotnik <micnowotnik@gmail.com>
2022-05-17 19:10:53 +0200

    errors commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-  async function fetchMoviesGandler() {
+    async function fetchMoviesGandler() {
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films');
-    const data = await response.json();
-    console.log(data);
+    setError(null);
 
-    const transformedMovies = data.results.map(movieData =>{
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+      const data = await response.json();
+
+
+      console.log(data);
+  
+      const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
           title: movieData.title,
           openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
+          releaseDate: movieData.release_date,
         };
       });
       setMovies(transformedMovies);
-      setIsLoading(false);
-    };
+    } catch (error) {
+        setError(error.message);
+        
+    }
+    setIsLoading(false);
+  }
 
+  let content = <p>Found no movies.</p>
+  if (movies.length > 0){
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <React.Fragment>
@@ -32,9 +56,7 @@ function App() {
         <button onClick={fetchMoviesGandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies</p>}
-        {isLoading && <p>Loading...</p>}
+        {content}
       </section>
     </React.Fragment>
   );
